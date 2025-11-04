7eca8d0ecbac769e6f6308d298b764719aa9805c
kabeerx9 <kabeer786joshi@gmail.com>
2025-11-04 22:37:44 +0530

    prettier

---
+++ app/components/practice.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { motion } from "motion/react";
+import { useState } from "react";
+
+export default function Practice() {
+  return (
+    <div className="h-full w-full flex items-center justify-center bg-linear-to-br from-zinc-900 via-zinc-800 to-black p-8">
+      <p className="text-white">Hello Kabeer</p>
+    </div>
+  );
+}
